e93f8633a528d5839c155d8efd392fdbb9d31572
alireza <ali.d.f.2012@gmail.com>
2022-10-15 12:37:00 +0330

    implement new loss with generative data

---
+++ core/data/cifar10g.py
@@ -0,0 +1,40 @@
+import torch
+
+import torchvision
+import torchvision.transforms as transforms
+import numpy as np
+
+
+class GENERATIVECIFAR10(torch.utils.data.Dataset):
+
+    def __init__(self, each_class=5000):
+        self.data = torch.Tensor(np.load("/content/cifar10_training_gen_data.npy"))
+        self.target = torch.Tensor([i // each_class for i in range(len(self.data))])
+
+    def __len__(self):
+        return len(self.target)
+
+    def __getitem__(self, index):
+        return self.data[index], self.target[index]
+
+
+# DATA_DESC = {
+#     'data': 'cifar10',
+#     'classes': ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck'),
+#     'num_classes': 10,
+#     'mean': [0.4914, 0.4822, 0.4465],
+#     'std': [0.2023, 0.1994, 0.2010],
+# }
+
+
+def load_cifar10_generative(data_dir=None, use_augmentation=False, each_class=5000):
+    """
+    Returns CIFAR10 train, test datasets and dataloaders.
+    Arguments:
+        data_dir (str): path to data directory.
+        use_augmentation (bool): whether to use augmentations for training set.
+    Returns:
+        train dataset, test dataset.
+    """
+
+    return GENERATIVECIFAR10()
